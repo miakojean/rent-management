fbea76c4d1393bbf1ab6b2bc19b5f27641ef5bdf
miakojean <miakojeanyves@gmail.com>
2026-01-21 11:50:23 +0000

    work on first section

---
+++ rent-management-front/app/components/navigation/navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav class="main__navbar">
+        <button>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
+            </svg>
+        </button>
+    </nav>
+</template>
+
+<script lang="ts"></script>
+
+<style scoped>
+.main__navbar{
+    width: 100%;
+    height: 60px;
+    border-bottom: 1px solid #ddd;
+    display: flex;
+    justify-content: end;
+}
+</style>
---
+++ rent-management-front/app/components/sections/indexSection.vue
@@ -0,0 +1,26 @@
+<template>
+    <section class="main__section">
+        <!-- Content for the index section goes here -->
+        <h1>
+            Welcome to the Rent Management System
+        </h1>
+    </section>
+</template>
+
+<script lang="ts">
+export default {
+    name: "IndexSection",
+    components: {
+        
+    }
+}
+</script>
+
+<style scoped>
+.main__section{
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: normal;
+}
+</style>
